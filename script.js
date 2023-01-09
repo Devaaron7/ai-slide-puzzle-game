e59b132209f4662a56f0f1e15b6012408d5636a8
Devaaron7 <lvluphero123+dev@gmail.com>
2023-01-08 23:46:16 -0500

    starting scripting logic

---
+++ script.js
@@ -0,0 +1,14 @@
+var finalTile = document.getElementById("P9of9");
+
+function move(element) {
+  var tileToMoveId = element.id;
+  var tileToMove = document.getElementById(tileToMoveId);
+  //   var parent = element.parentNode;
+  //   alert(tileToMove);
+  //   var content = parent.querySelector("div");
+  // var content = parent
+  //   alert(content.id);
+  tileToMove.style.left = "354px";
+  //   console.log(tileToMove.style.left = 354);
+  //   console.log(tileToMove.style.top);
+}
