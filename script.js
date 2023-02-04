7476e001a1ad75d914506c74d04568fbf7266940
Link <118638677+linkthedev90@users.noreply.github.com>
2023-02-04 10:40:02 -0500

    working on adding word animation

--- script.js
+++ script.js
@@ -17,6 +17,66 @@ var sfx = new Audio(
 
 sfx.load();
 
+// Animation
+var ml4 = {};
+ml4.opacityIn = [0, 1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+// prettier-ignore
+anime.timeline({ loop: true })
+  .add({
+    targets: ".ml4 .letters-1",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-1",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4 .letters-2",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-2",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4 .letters-3",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-3",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4",
+    opacity: 0,
+    duration: 500,
+    delay: 500,
+  });
+
 // variables & functions used in testing
 let listOfWinningPattern = [];
 let listOfCurrentPattern = [];
